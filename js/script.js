743376ddf24fafbf022ca3d526f64c75b87cfb06
antoniovenezia <antoniovenezia00@gmail.com>
2021-06-30 10:17:29 +0200

    vue-boolzapp

---
+++ js/script.js
@@ -0,0 +1,15 @@
+Vue.config.devtools = true;
+
+new Vue(
+    {
+        el: '#app',
+        data: {
+            // qui dentro metteremo tutti i dati
+            // della nostra applicazione.
+            // la nostra app si attiverà e verrà 
+            // eseguita in funzione di...
+            text: "ciaone",
+        },
+    })
+
+console.log();
